79fa025c3b0756f861d0bf1a9d5d13d10f8f7774
harrisonford <kristofher@ug.uchile.cl>
2021-06-05 19:59:35 -0400

    moved to python fodler

---
+++ Solidity/HelloWorld.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.6.6;
+
+contract HelloWorld {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function updateMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
